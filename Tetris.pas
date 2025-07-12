bf65e4846c72b3c581a26e198e00b7cb0a83cdd4
sjlindvall <sjlindvall@live.com>
2025-07-12 19:28:20 +0200

    flere tetriminos

--- Tetris.pas
+++ Tetris.pas
@@ -15,20 +15,25 @@ type
   TFormTetrisGame = class(TForm)
     ButtonDrop: TButton;
     ButtonRoter: TButton;
+    ButtonLeft: TButton;
+    ButtonRight: TButton;
     procedure FormCreate(Sender: TObject);
     procedure ButtonDropClick(Sender: TObject);
     procedure ButtonRoterClick(Sender: TObject);
     procedure AnimationDone(Sender: TObject);
     procedure RotationDone(Sender: TObject);
     procedure DropNextStep(Sender: TObject);
+    procedure ButtonLeftClick(Sender: TObject);
+    procedure ButtonRightClick(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char; Shift: TShiftState);
 
   private
     { Private declarations }
     procedure CreateIshape(thisShape : TFMXObject; rotation:TRotation);
     procedure CreateOshape(thisShape: TFMXObject);
     procedure CreateTshape(thisShape : TFMXObject; rotation:TRotation);
-    procedure CreateSshape(centerX,centerY:Single; rotation:TRotation);
-    procedure CreateZshape(centerX,centerY:Single; rotation:TRotation);
+    procedure CreateSshape(thisShape : TFMXObject; rotation:TRotation);
+    procedure CreateZshape(thisShape : TFMXObject; rotation:TRotation);
     procedure CreateJshape(centerX,centerY:Single; rotation:TRotation);
     procedure CreateLshape(centerX,centerY:Single; rotation:TRotation);
     function CreateShape(tetriMino:TShape; centerX,centerY:Single; rotation:TRotation): TLayout;
@@ -36,7 +41,6 @@ type
     procedure SetupRotation;
     procedure DoRotation;
     function NextRotation(R: TRotation): TRotation;
-    {procedure SetupDrop;}
 
   public
     { Public declarations }
@@ -70,6 +74,8 @@ end;
 
 procedure TFormTetrisGame.FormCreate(Sender: TObject);
 begin
+  Self.OnKeyDown := FormKeyDown;
+
   // Lag timer
   DropTimer := TTimer.Create(Self);
   DropTimer.Interval := 500;           // 500 ms = én drop per halvt sekund
@@ -97,7 +103,7 @@ begin
   Rect.Fill.Gradient.StopPosition.X := 1;
   Rect.Fill.Gradient.StopPosition.Y := 1;
 
-  theShape := CreateShape(TShape.T, 100,100, TRotation.R90);
+  theShape := CreateShape(TShape.Z, 100,100, TRotation.R0);
   theShape.RotationCenter.X := 0.5;
   theShape.RotationCenter.Y := 0.5;
 
@@ -121,9 +127,29 @@ begin
   theShape.Position.Y := theShape.Position.Y + BLOCK_SIZE;
 end;
 
+procedure TFormTetrisGame.FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char; Shift: TShiftState);
+begin
+  case Key of
+    vkLeft:  theShape.Position.X := theShape.Position.X - BLOCK_SIZE;
+    vkRight: theShape.Position.X := theShape.Position.X + BLOCK_SIZE;
+    vkUp:    DoRotation;
+    vkDown:  theShape.Position.Y := theShape.Position.Y + BLOCK_SIZE;
+  end;
+end;
+
 procedure TFormTetrisGame.ButtonDropClick(Sender: TObject);
 begin
   Animation.Enabled := True;
+end;
+
+procedure TFormTetrisGame.ButtonLeftClick(Sender: TObject);
+begin
+  theShape.Position.X := theShape.Position.X - BLOCK_SIZE;
+end;
+
+procedure TFormTetrisGame.ButtonRightClick(Sender: TObject);
+begin
+  theShape.Position.X := theShape.Position.X + BLOCK_SIZE;
 end;
 
 procedure TFormTetrisGame.ButtonRoterClick(Sender: TObject);
@@ -204,28 +230,28 @@ begin
   CreateBlock(thisShape, 1, 0, TAlphaColorRec.Green);
 end;
 
+procedure TFormTetrisGame.CreateSshape(thisShape : TFMXObject; rotation:TRotation);
+begin
+  CreateBlock(thisShape, 0, 1, TAlphaColorRec.Red);
+  CreateBlock(thisShape, 1, 1, TAlphaColorRec.Red);
+  CreateBlock(thisShape, 1, 0, TAlphaColorRec.Red);
+  CreateBlock(thisShape, 2, 0, TAlphaColorRec.Red);
+end;
+
+procedure TFormTetrisGame.CreateZshape(thisShape : TFMXObject; rotation:TRotation);
+begin
+  CreateBlock(thisShape, 0, 0, TAlphaColorRec.Purple);
+  CreateBlock(thisShape, 1, 0, TAlphaColorRec.Purple);
+  CreateBlock(thisShape, 1, 1, TAlphaColorRec.Purple);
+  CreateBlock(thisShape, 2, 1, TAlphaColorRec.Purple);
+end;
+
 procedure TFormTetrisGame.CreateLshape(centerX,centerY:Single; rotation:TRotation);
 begin
   CreateBlock(Self, 100, 200, TAlphaColorRec.Orange);
   CreateBlock(Self, 130, 200, TAlphaColorRec.Orange);
   CreateBlock(Self, 160, 200, TAlphaColorRec.Orange);
   CreateBlock(Self, 160, 170, TAlphaColorRec.Orange);
-end;
-
-procedure TFormTetrisGame.CreateZshape(centerX,centerY:Single; rotation:TRotation);
-begin
-  CreateBlock(Self, 100, 400, TAlphaColorRec.Red);
-  CreateBlock(Self, 130, 400, TAlphaColorRec.Red);
-  CreateBlock(Self, 130, 370, TAlphaColorRec.Red);
-  CreateBlock(Self, 160, 370, TAlphaColorRec.Red);
-end;
-
-procedure TFormTetrisGame.CreateSshape(centerX,centerY:Single; rotation:TRotation);
-begin
-  CreateBlock(Self, 260, 400, TAlphaColorRec.Purple);
-  CreateBlock(Self, 230, 400, TAlphaColorRec.Purple);
-  CreateBlock(Self, 230, 370, TAlphaColorRec.Purple);
-  CreateBlock(Self, 200, 370, TAlphaColorRec.Purple);
 end;
 
 procedure TFormTetrisGame.CreateJshape(centerX,centerY:Single; rotation:TRotation);
@@ -269,8 +295,24 @@ begin
         tetrisShape.Position.Y := centerY - (tetrisShape.Height / 2);
         CreateTshape(tetrisShape, rotation);
       end;
-    S: ;
-    Z: ;
+    S:
+      begin
+        tetrisShape.Width := BLOCK_SIZE * 3;
+        tetrisShape.Height := BLOCK_SIZE * 2;
+        tetrisShape.Position.X := centerX - (tetrisShape.Width / 2);
+        tetrisShape.Position.Y := centerY - (tetrisShape.Height / 2);
+        CreateSshape(tetrisShape, rotation);
+      end;
+
+    Z:
+      begin
+        tetrisShape.Width := BLOCK_SIZE * 3;
+        tetrisShape.Height := BLOCK_SIZE * 2;
+        tetrisShape.Position.X := centerX - (tetrisShape.Width / 2);
+        tetrisShape.Position.Y := centerY - (tetrisShape.Height / 2);
+        CreateZshape(tetrisShape, rotation);
+      end;
+
     J: ;
     L: ;
   end;
